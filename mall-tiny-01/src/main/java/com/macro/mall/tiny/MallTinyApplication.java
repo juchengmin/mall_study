4d10c48c4672fe4b4770a44202e528c19849b7b1
jcm <1097204413@qq.com>
2023-05-11 14:10:44 +0800

    测试使用idea提交git的修改

--- mall-tiny-01/src/main/java/com/macro/mall/tiny/MallTinyApplication.java
+++ mall-tiny-01/src/main/java/com/macro/mall/tiny/MallTinyApplication.java
@@ -3,6 +3,9 @@ package com.macro.mall.tiny;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 测试用idea提交git
+ */
 @SpringBootApplication
 public class MallTinyApplication {
 
